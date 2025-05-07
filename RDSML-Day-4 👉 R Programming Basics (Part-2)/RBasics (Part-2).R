270da8a58798e18ee451ebbb9a562a3b10f94b21
shahjohir <sohagag@gmail.com>
2025-05-07 22:50:32 +0600

    Day 2 Practice

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/RBasics (Part-2).R
@@ -0,0 +1,17 @@
+# data Tupes in R
+# 1. Numeric -Integer
+# 2. String-"Sadhan Verma" / 'Sadhan Verma"
+# 3. Logical
+
+a = 11
+b = 7.19
+
+
+
+
+# Logical Operators in R
+# && logical Operator
+# 
+4 == 4 && 5==5
+X != y || X == Y
+
