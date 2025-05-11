4409fb2886299e4ddce60776a39f7922d7cba496
shahjohir <sohagag@gmail.com>
2025-05-11 14:44:19 +0600

    RBasics (Part2)

--- RDSML-Day-4 👉 R Programming Basics (Part-2)/RBasics (Part-2).R
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/RBasics (Part-2).R
@@ -1,17 +1,49 @@
-# data Tupes in R
-# 1. Numeric -Integer
-# 2. String-"Sadhan Verma" / 'Sadhan Verma"
+# data Types in R
+# 1. Numeric -Integer/Floating point
+# 2. String/Character-"Sadhan Verma" / 'Sadhan Verma"
 # 3. Logical
 
 a = 11
 b = 7.19
+class(a)
+class(b)
+name = 'Shah Johir Rayhan'
+print(name)
+class(name)
+c = '52'
+print(c + 9)
+print(a + 9)
+class(c)
 
-
-
+# Logical Data Type- True, False (T, F)
+a > b
+class(a > b)
+x = 19
+y = 22-3
+print(x == y)
+print(x != y)
+print(y > x)
+print(y >= x)
 
 # Logical Operators in R
-# && logical Operator
-# 
-4 == 4 && 5==5
-X != y || X == Y
+# && Logical Operator
+
+4 == 4 && 5 == 5
+class(4 == 4 && 5 == 5)
+
+# || Logical Operator
+
+x != y || x == y
+
+# Simple Mathematical Operators in R
+z = 6 + 9 - 8*(2/3)
+print(z)
+
+
+
+
+
+
+
+
 
